f73ead41df72477ae1cd093efcde94486ec7de86
Arnau Mora Coll <143502251+amoracoll@users.noreply.github.com>
2024-10-13 20:34:49 +0200

    Added routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
+import routes from "./routes/index";
+
 const app = express();
 const port = 3000;
 
-app.get("/api", (req, res) => {
-    res.send("Hello world!")
-});
+app.use('/api', routes);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
