ed95c6eb1a5b155830742a94a5ab30b9802bb0e3
arnaumoracoll <arnaumoracoll@gmail.com>
2024-10-17 11:49:16 +0200

    Added csvToJson types and finished exercise

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import routes from "./routes/index";
 import student from "./routes/student";
 import teacher from "./routes/teacher";
 import runner from "./routes/runner";
+import convertCsvToJsonExercise from "./routes/csvToJsonExercise";
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,7 @@ app.use('/api', routes);
 app.use('/student', student);
 app.use('/teacher', teacher);
 app.use('/runner', runner);
+app.use('/convert', convertCsvToJsonExercise)
 
 
 app.listen(port, () => {
