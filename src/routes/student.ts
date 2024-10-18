3003734b9ee9bb5889c3b5136b6cb8acf700d0e3
arnaumoracoll <arnaumoracoll@gmail.com>
2024-10-18 09:21:28 +0200

    Deteled all files

--- src/routes/student.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const student = express.Router();
-
-student.get("/", (req, res) => {
-    res.send("I'm a student!")
-});
-
-export default student;
--- src/routes/teacher.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const teacher = express.Router();
-
-teacher.get("/", (req, res) => {
-    res.send("I'm a teacher!")
-});
-
-export default teacher;
