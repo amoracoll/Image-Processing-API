2e451fec074ef70ff93bec13205b08e4a7fbe8dc
arnaumoracoll <arnaumoracoll@gmail.com>
2024-10-23 16:39:20 +0200

    Added functionality to load images to browser

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import path from "path";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello world!");
+// Servir archivos estáticos de la carpeta "assets/images"
+app.use("/assets", express.static(path.join(__dirname, "../assets")));
+
+// Ruta para mostrar la imagen
+app.get("/", (req: Request, res: Response) => {
+    // Imprimir la ruta
+    console.log(path.join(__dirname, "../assets","santamonica.jpg"));
+    res.send(`
+        <h1>Imagen desde Express</h1>
+        <img src="/assets/santamonica.jpg" alt="Mi Imagen" />
+    `);
 });
 
 app.listen(port, () => {
